295cfc1932ebc64606e936c4a2af4329587e8b16
kana <kanabanarama@googlemail.com>
2017-07-15 15:47:52 +0200

    animated icons

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 $(document).foundation();
+
+// CSS Animations
+function CssAnimations() {
+  var animatedElements = $('.animated:visible');
+  var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+  animatedElements.each(function(index, element) {
+    // Popup animation triggered when icon is scrolled into view
+    if($(element).attr('class').indexOf('animate-view-popup') !== -1) {
+      var viewportTop = $(scrollElem).scrollTop();
+      var viewportBottom = viewportTop + $(window).height();
+      var elementTop = Math.round($(element).offset().top);
+      if(elementTop +100 < viewportBottom) {
+        $(element).addClass('animate-popup')
+      }
+    }
+  });
+}
+
+window.addEventListener('scroll', CssAnimations);
