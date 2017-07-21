ca8cd37e45fa5352bbac5ccdafe8fc6817d53a0b
kana <kanabanarama@googlemail.com>
2017-07-21 23:01:37 +0200

    Experimenting with animations, made an icon catapult

--- js/app.js
+++ js/app.js
@@ -26,19 +26,23 @@ var posY = window.pageYOffset;
 
 var svg = d3
   .select('#svg-1')
-  .append('svg')
   .attr('width', '100%')
-  .attr('height', '2500')
+  .attr('height', '2000');
 
 var path = [
   [w/2-5, 200],
-  [w/2, 300],
-  [w/2-50, 400],
-  [w/2+50, 500],
-  [w/2-50, 600],
-  [w/2, 550],
-  [w/2+300, 900],
-  [w/2+300, 5000],
+  [w/2+30, 300],
+  [w/2-70, 400],
+  [w/2+100, 520],
+  [w/2-100, 620],
+  [w/2-960, 660],
+  [w/2-940, 390],
+  [w/2+50, 450],
+  [w/2+160, 1410],
+  [w/2+190, 1410],
+  [w/2+450, 1300],
+  [w/2+500, 1300],
+  [w/2+1000, 5000]
 ];
 
 var bezierLine = d3
@@ -63,72 +67,159 @@ var stroke = svg.selectAll('liner')
   .attr('stroke-width', 10)
   .attr('fill', 'none')
   .attr('stroke-linecap', 'round')
-  .attr('stroke-dasharray', function(d) {
+  .attr('stroke-dasharray', function() {
     var strokeLength = this.getTotalLength();
     progress = this.getTotalLength()-posY;
     return strokeLength+' '+strokeLength;
   })
-  .attr('stroke-dashoffset', function(d) {
-    return this.getTotalLength()-posY
+  .attr('stroke-dashoffset', function() {
+    return this.getTotalLength()-posY;
   });
 
 var animatePath = function() {
   stroke
-    .attr('stroke-dashoffset', function(d) {
+    .attr('stroke-dashoffset', function() {
       //var forwardOnlyProgress = Math.min(progress, this.getTotalLength()-posY);
       //var progressWithLimit = Math.max(0, forwardOnlyProgress);
-      var catchUpFactor = 1.2;
+      var catchUpFactor = 5;
       var progress = this.getTotalLength()-posY*catchUpFactor;
       return progress;
     });
 }
 
 // Scroll bound sectional animations
-var testData = [{'id': 1}];
-
 var keyframes = [
   {
-    'position': 300,
-    'start': function() {
+    position: 636,
+    selector: '.test rect',
+    remove: false,
+    start: function() {
       return svg
-        .selectAll('circle')
-        .data(testData, function(d) { return d.id; })
-        .enter()
-          .append('circle')
-          .attr('r', 25) //30
-          .attr('cx', w/2-15)
-          .attr('cy', 570)
-          .attr('fill', '#FFFFFF');
+        .append('g')
+        .attr('class', 'test')
+        .attr('transform', 'translate('+(w/2-300)+', 1400)')
+        .append('rect')
+        .attr('width', 600)
+        .attr('height', 50)
+        .attr('opacity', 1)
+        .attr('fill', '#FFFFFF');
     },
-    'end': function() {
+    end: function() {
       return svg
-        .selectAll('circle')
-        .data(testData, function(d) { return d.id; })
-          .transition()
-          .duration(300)
-          .attr('r', 20)
-          .attrTween('fill', function() {
-            return function(t) {
-              return (d3.interpolateRgb('#FFFFFF', '#FF6666'))(t)
-            };
-          });
+        .select('.test rect')
+        .transition()
+        .duration(2000)
+        .ease(d3.easeBounce)
+        .attrTween('transform', function() {
+          var i = d3.interpolate(0, 180);
+          return function(t) {
+            return 'rotate(' + i(t) + ', 300, 25)';
+          };
+        });
+    }
+  },
+  {
+    position: 636,
+    selector: '#icon-monitor',
+    remove: true,
+    start: function() {
+      return svg
+        .select('#icon-monitor')
+        .attr('transform', 'translate('+(w/2-300)+', 1330)');
+    },
+    end: function() {
+      return svg
+        .select('#icon-monitor')
+        .transition()
+        .duration(700)
+        .attr('transform', 'translate('+(w-85)+', 900)')
+        .ease(d3.easeQuadOut)
+        .transition()
+        .duration(2000)
+        .attr('transform', 'translate('+(w/2-300)+', 2500)')
+        .ease(d3.easeQuadInOut);
+    }
+  },
+  {
+    position: 636,
+    selector: '#icon-database',
+    remove: true,
+    start: function() {
+      return svg
+        .select('#icon-database')
+        .attr('transform', 'translate('+(w/2-100-20)+', 1330)');
+    },
+    end: function() {
+      return svg
+        .select('#icon-database')
+        .transition()
+        .duration(800)
+        .attr('transform', 'translate('+(w-85)+', 1000)')
+        .ease(d3.easeQuadOut)
+        .transition()
+        .duration(2000)
+        .attr('transform', 'translate('+(w/2-100)+', 2500)')
+        .ease(d3.easeQuadInOut);
+    }
+  },
+  {
+    position: 636,
+    selector: '#icon-cloud',
+    remove: true,
+    start: function() {
+      return svg
+        .select('#icon-cloud')
+        .attr('transform', 'translate('+(w/2+100-40)+', 1330)');
+    },
+    end: function() {
+      return svg
+        .select('#icon-cloud')
+        .transition()
+        .duration(2000)
+        .attr('transform', 'translate('+(w/2+300)+', 2500)')
+        .ease(d3.easeQuadInOut);
+    }
+  },
+  {
+    position: 636,
+    selector: '#icon-app',
+    remove: true,
+    start: function() {
+      return svg
+        .select('#icon-app')
+        .attr('transform', 'translate('+(w/2+300-80)+', 1330)');
+    },
+    end: function() {
+      return svg
+        .select('#icon-app')
+        .transition()
+        .duration(2000)
+        .attr('transform', 'translate('+(w/2+500)+', 2500)')
+        .ease(d3.easeQuadInOut);
     }
   }
 ];
 
 var initSections = function() {
-  keyframes.forEach(function(keyframe, index) {
-    keyframe.start();
-    if(window.pageYOffset > keyframe.position && !keyframe.status) {
-      keyframe.end().duration(0).attr('data-animation-finished');
+  keyframes.forEach(function(keyframe) {
+    keyframe.start().attr('visibility', 'visible');
+    if(window.pageYOffset >= keyframe.position && !keyframe.status) {
+      var keyframeElement = svg
+        .select(keyframe.selector)
+        .interrupt()
+      if(keyframe.remove) {
+        keyframeElement.remove()
+      } else {
+        keyframe.end().duration(0).attr('data-animation-finished');
+      }
       keyframe.status = 1;
     }
   });
 }
 
 var animateSections = function() {
-  keyframes.forEach(function(keyframe, index) {
-    if(window.pageYOffset > keyframe.position && !keyframe.status) {
+  keyframes.forEach(function(keyframe) {
+    if(window.pageYOffset >= keyframe.position && !keyframe.status) {
       keyframe.end().attr('data-animation-finished');
       keyframe.status = 1;
     }
@@ -146,29 +237,42 @@ d3.select(window).on('scroll.scroller', function() {
 // Helper that shows x/y position of the cursor
 var initPositionHelper = function() {
   var positionDisplay = d3
-    .select("body")
-    .append("div")
+    .select('body')
+    .append('div')
     .attr('data-allow-html', true)
     .attr('class', 'tooltip top')
     .style('display', 'none');
   var centerOffsetDisplay = d3
-    .select("body")
-    .append("div")
+    .select('body')
+    .append('div')
     .attr('class', 'tooltip right')
     .style('display', 'none');
+  var scrollDisplay = d3
+    .select('body')
+    .append('span')
+    .attr('class', 'tooltip bottom')
+    .style('width', '200px')
+    .style('text-align', 'center');
 
-  d3.select(window).on("mousemove", function() {
+  d3.select(window).on('mousemove', function() {
     positionDisplay
       .html('x: ' + d3.event.pageX + '<br /> y: ' + d3.event.pageY)
-      .style("top", (d3.event.pageY - 100) + "px")
-      .style("left", (d3.event.pageX - 34) + "px")
+      .style('top', (d3.event.pageY - 100) + 'px')
+      .style('left', (d3.event.pageX - 34) + 'px')
       .style('display', 'inline');
     var centerOffset =  d3.event.pageX - w/2;
     centerOffsetDisplay
       .html('center: ' + centerOffset)
-      .style("top", (d3.event.pageY - 25) + "px")
-      .style("left", (d3.event.pageX + 35) + "px")
+      .style('top', (d3.event.pageY - 25) + 'px')
+      .style('left', (d3.event.pageX + 35) + 'px')
       .style('display', 'inline');
+  });
+
+  d3.select(window).on('scroll', function() {
+    scrollDisplay
+      .html('scroll: ' + posY)
+      .style('top', (posY + 100) + 'px')
+      .style('left', ($(window).width()- 200) + 'px')
   });
 }
 
