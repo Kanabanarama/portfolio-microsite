311cc7848c00aff3e631ce85a108bcc2b3f7158a
Kana <kanabanarama@googlemail.com>
2017-12-23 00:02:04 +0100

    Updated webpack config for production builds

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,29 @@ var webpack = require('webpack');
 
 module.exports = {
     context: __dirname,
-    devtool: debug ? "inline-sourcemap" : null,
+    devtool: debug ? "inline-sourcemap" : false,
     entry: "./js/app.js",
     output: {
         path: __dirname + "/js",
         filename: "app.min.js"
     },
     plugins: debug ? [] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
     ],
     module: {
-      loaders: [{
-          test: /\js\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-      }]
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015'],
+                        sourceMap: false,
+                    },
+                },
+            },
+        ],
     }
 };
